50d1d54824c9a73b219f05ceac527b2b45e3a80f
Jesus Carpintero <jesus.carpintero@adevinta.com>
2022-11-28 12:05:25 +0100

    Add target-image-refs

--- run.sh
+++ run.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
 VL_ARGS=()
+
+# Setting log level
 if [ -n "${LOG_LEVEL}" ]; then
     VL_ARGS+=("-l" "${LOG_LEVEL}")
 fi
-echo "vulcan-local -t . ${VL_ARGS[*]}"
-vulcan-local -t . "${VL_ARGS[@]}"
+
+# Adding images
+if [ -n "${TARGET_IMAGE_REFS}" ]; then
+    for IMG in ${TARGET_IMAGE_REFS}; do
+        VL_ARGS+=("-t" "${IMG}" "-a" "DockerImage")
+    done
+fi
+
+# Adding local repo
+VL_ARGS+=("-t" "." "-a" "GitRepository")
+
+echo "vulcan-local ${VL_ARGS[*]}"
+vulcan-local "${VL_ARGS[@]}"
