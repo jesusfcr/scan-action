291772fd177cd6c188484546c1c319b566187d45
Jesus Carpintero <jesus.carpintero@adevinta.com>
2022-11-28 11:29:03 +0100

    Fix if

--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 VL_ARGS=()
-if [ -e "${LOG_LEVEL}" ]; then
+if [ -n "${LOG_LEVEL}" ]; then
     VL_ARGS+=("-l" "${LOG_LEVEL}")
 fi
-echo "log-level ${LOG_LEVEL}"
 echo "vulcan-local -t . ${VL_ARGS[*]}"
 vulcan-local -t . "${VL_ARGS[@]}"
